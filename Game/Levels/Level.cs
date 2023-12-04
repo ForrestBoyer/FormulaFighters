2242e45e6aefb4c3a6c0d4cad8380ddaa53d6756
Forrest Boyer <forrestboyer2@gmail.com>
2023-12-04 16:55:22 -0500

    correctly loads 1 of 3 random backgrounds now

---
+++ Game/Levels/Level.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Level : Node2D
+{
+	public TextureRect Background { get; set; }
+	public Random rand = new Random();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Background = GetNode<TextureRect>("Background");
+
+		string[] texturePaths = 
+		{
+			"res://Game/Levels/Backgrounds/brownBricksRoom.png",
+			"res://Game/Levels/Backgrounds/darkBlueRoom.png",
+			"res://Game/Levels/Backgrounds/throneRoom.png",
+		};
+
+		int index = rand.Next(texturePaths.Length);
+		var texture = (Texture2D)GD.Load(texturePaths[index]);
+
+		GD.Print(texture.ResourcePath);
+
+		if (texture != null)
+		{
+			Background.Texture = texture;
+		}
+		else
+		{
+			GD.Print("Faied to load texture");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
